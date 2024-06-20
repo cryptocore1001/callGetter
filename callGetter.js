198525a627716bc2b88ba3fdfb89800b5207152d
cryptocore1001 <crutugremidoi-9188@ac-cool.c4.fr>
2024-06-21 01:51:45 +1000

    modified callGetter file

--- callGetter.js
+++ callGetter.js
@@ -1,43 +1,28 @@
-import buffer from "buffer";
-import {
-  TonClient,
-  TonClient4,
-  WalletContractV4,
-  Contract,
-  ContractProvider,
-  Address,
-  Cell,
-  beginCell,
-  storeStateInit,
-  JettonMaster,
-  JettonWallet,
-  OpenedContract,
-} from "@ton/ton";
-import { getHttpEndpoint, getHttpV4Endpoint } from "@orbs-network/ton-access";
+import { TonClient, Address, Cell, beginCell, storeStateInit } from "@ton/ton";
+import { getHttpV4Endpoint } from "@orbs-network/ton-access";
 
-// address link: https://tonviewer.com/EQBjZf-UZaz6YQTkHJ3EBxJpgh6GROxoecRm3vVNcncwHNy9
-const counterAddress = Address.parse(
-  "EQBjZf-UZaz6YQTkHJ3EBxJpgh6GROxoecRm3vVNcncwHNy9"
-);
+const counterAddress = Address.parse("EQBjZf-UZaz6YQTkHJ3EBxJpgh6GROxoecRm3vVNcncwHNy9");
 
-// Needs rpc endpoint
 async function callCounter() {
   try {
-    // create ton client
     const tonCenterEndpoint = await getHttpV4Endpoint({
       network: "mainnet",
     });
 
-    const client4 = new TonClient4({
+    const client = new TonClient({
       endpoint: tonCenterEndpoint,
     });
 
-    /* 
-    Add ton client code to call counter. It should return 1
-    
-    console.log to print the result
+    const query = {
+      address: counterAddress,
+      functionName: "counter",
+    };
 
-    */
+    const result = await client.query({
+      query,
+    });
+
+    console.log("Counter value:", result.value);
   } catch (error) {
     console.error(error);
   }
@@ -45,33 +30,28 @@ async function callCounter() {
 
 async function incrementCounter() {
   try {
-    // create ton client
     const tonCenterEndpoint = await getHttpV4Endpoint({
       network: "mainnet",
     });
 
-    const client4 = new TonClient4({
+    const client = new TonClient({
       endpoint: tonCenterEndpoint,
     });
 
-    let mnemonics = await mnemonicNew();
+    const message = {
+      address: counterAddress,
+      functionName: "increment",
+    };
 
-    let keyPair = await mnemonicToPrivateKey(mnenomics);
-
-    let workchain = 0; // Usually you need a workchain 0
-    let wallet = WalletContractV4.create({
-      workchain,
-      publicKey: keyPair.publicKey,
+    const result = await client.sendMessage({
+      message,
     });
-    let walletContract = client4.open(wallet);
 
-    /* 
-    Add ton client code to send 'increment' to the contract
-    */
+    console.log("Message sent. Transaction hash:", result.transaction.id);
   } catch (error) {
     console.error(error);
   }
 }
 
-// callCounter();
-// incrementCounter();
+callCounter();
+incrementCounter();
